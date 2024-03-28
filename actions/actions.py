94c27a6c8a7fc0cfc0943435a0c3d788af577e3b
unknown <guilizondo@gmail.com>
2024-03-28 13:26:43 -0700

    added weather feature

--- actions/actions.py
+++ actions/actions.py
@@ -27,6 +27,8 @@
 #         return []
 
 from typing import Any, Text, Dict, List
+
+import requests
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
@@ -52,3 +54,28 @@ class ActionProvideAdvice(Action):
                 text="Mmh... I don't know that one. It is probably better for you to go to the hospital!")
 
         return []
+
+
+class ActionProvideWeatherForecast(Action):
+
+    def name(self) -> Text:
+        return "action_provide_weather_forecast"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        city = tracker.get_slot('city')
+        api_key = "9de2c0d3a8ba2ab4dcfcc7cee516a701"
+        response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}")
+
+        if response.status_code == 200:
+            data = response.json()
+            temperature = data["main"]["temp"] - 275
+            weather = data["weather"][0]["description"]
+            dispatcher.utter_message(text=f"The current temperature in {city} is {temperature:.2f}°C. You can expect {weather}")
+        else:
+            dispatcher.utter_message(text=f"Couldn't retrieve the weather for {city}. Please try again.")
+
+        return []
+
