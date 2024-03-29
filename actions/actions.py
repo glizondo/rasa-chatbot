9ad18813201693581e8d05365a68897e1da3a338
PatterP <pattpay13@gmail.com>
2024-03-29 16:03:55 -0400

    Manual Merge

--- actions/actions.py
+++ actions/actions.py
@@ -78,4 +78,45 @@ class ActionProvideWeatherForecast(Action):
             dispatcher.utter_message(text=f"Couldn't retrieve the weather for {city}. Please try again.")
 
         return []
+        
+class ActionRecomendBook(Action):
 
+    def name(self) -> Text:
+        return "action_recomend_book"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        genre = tracker.get_slot('genre')
+
+        if genre == "Fantasy":
+            dispatcher.utter_message(text="You might like The Dark Tower by Stephen King")
+        elif genre == "Sci-fi":
+            dispatcher.utter_message(text="You might like Dune by Frank Herbert")
+        else:
+            dispatcher.utter_message(
+                text="Sorry, I can't think of any books at the moment.")
+
+        return []
+ 
+class ActionBookFlight(Action):
+
+    def name(self) -> Text:
+        return "action_book_flight"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        city = tracker.get_slot('city')
+
+        if city == "Grand Rapids":
+            dispatcher.utter_message(text="Got it. Booking a flight from GRR Airport.")
+        elif city == "Chicago":
+            dispatcher.utter_message(text="Got it. Booking a flight from O'Hare Airport.")
+        else:
+            dispatcher.utter_message(
+                text="Sorry, I don't have that city and its airports in my database.")
+
+        return []
